2cd5a237db91433b5cfcfde1a9ba022a2dc7e707
victor <victorferrero1303@gmail.com>
2023-10-27 13:03:54 +0200

    ya no dice mis apellidos pero dice mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba{
 
 	public static void main(String args[]){
 
-		System.out.println("Ferrero vega");
 		System.out.println("tengo 20 años y naci el 2003");
 		System.out.println("me gustan las crocretas");
+		System.out.println("Mi cantante favorito actual es Ren");
 	}
 	
 }
